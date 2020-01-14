bf85c03d6a6f8d740c0a579db97cd244cf589f81
vedhagiri <giri@brewengage.com>
2020-01-14 06:20:41 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Hi Buy There!!! Welcome to our code Thanks to view</p>
         <a
           className="App-link"
           href="https://reactjs.org"
