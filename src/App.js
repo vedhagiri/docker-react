ebfd5ba59ec67e894cf696d607ff610603d73b3b
vedhagiri <giri@brewengage.com>
2020-01-14 08:12:53 +0530

    changes app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Buy There!!! Welcome to our code Thanks to view</p>
+          I was changes on the feature branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
